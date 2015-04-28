501d39d182222d68399a1d3cb0a6a27291e17f3a
therewasaguy <jason.sigal@gmail.com>
2015-04-28 00:50:31 -0400

    play samples instead of synths

--- www/js/sketch.js
+++ www/js/sketch.js
@@ -8,7 +8,10 @@ var y_2003 = 10;
 var speed = 1;
 var distance;
 
-var synths = new Array(4);
+// var synths = new Array(4);
+var samplers = new Array(4);
+var samplerNames = ['balafon', 'bongo', 'dink', 'panflute', 'triangle'];
+
 var counters = [new Array(100), new Array(100), new Array(100), new Array(100)];
 var indexes = [0, 0, 0, 0];
 // var indexModulos = [20, 33, 10, 66]; // good for tests
@@ -69,10 +72,23 @@ function draw() {
 
 function initSound() {
 
-  for (var i = 0; i < synths.length; i++) {
-    synths[i] = new Tone.MonoSynth();
-    synths[i].oscillator.type = "sine";
-    synths[i].toMaster();
+  // synths
+  // for (var i = 0; i < synths.length; i++) {
+  //   synths[i] = new Tone.MonoSynth();
+  //   synths[i].oscillator.type = "sine";
+  //   synths[i].toMaster();
+  // }
+
+  // samplers
+  for (var i = 0; i < samplers.length; i++) {
+    var samplerName = samplerNames[i];
+    samplers[i] = new Tone.Sampler ( [
+      "../www/audio/" + samplerName + "_01.mp3",
+       "../www/audio/" + samplerName + "_02.mp3", "../www/audio/" + samplerName + "_03.mp3", 
+      "../www/audio/" + samplerName + "_04.mp3"
+    ]
+    );
+    samplers[i].toMaster();
   }
 
   Tone.Transport.setInterval(play2000, "32n");
@@ -80,7 +96,11 @@ function initSound() {
   Tone.Transport.setInterval(play2002, "32n");
   Tone.Transport.setInterval(play2003, "32n");
 
-  Tone.Transport.start();
+  // start once all the audio buffers have loaded
+  Tone.Buffer.onload = function() {
+    console.log('loaded buffer');
+    Tone.Transport.start();
+  }
 
 }
 
@@ -98,30 +118,49 @@ function initSound() {
 
 
 function play2000(time) {
+  var t = time || Tone.Transport.now();
   var i = indexes[0]++ % indexModulos[0];
   if (counters[0][i] === 1 ) {
-    synths[0].triggerAttackRelease('C4', 0.12, Tone.Transport.now(), 0.1 );
+    var whichSample = Math.floor(Math.random()*4);
+    var velocity = map( Math.random(), 0, 1, 0.5, 0.75);
+    samplers[0].triggerAttack(whichSample, t, velocity);
+    // synths[0].triggerAttackRelease('C4', 0.12, Tone.Transport.now(), 0.1 );
   }
 }
 
 function play2001(time) {
+  var t = time || Tone.Transport.now();
   var i = indexes[1]++ % indexModulos[1];
   if (counters[1][i] === 1 ) {
-    synths[1].triggerAttackRelease('E4', 0.1, Tone.Transport.now(), 0.1 );
+    var whichSample = Math.floor(Math.random()*4);
+    var velocity = map( Math.random(), 0, 1, 0.5, 0.75);
+    samplers[1].triggerAttack(whichSample, t, velocity);
+
+    // synths[1].triggerAttackRelease('E4', 0.1, Tone.Transport.now(), 0.1 );
   }
 }
 
 function play2002(time) {
+  var t = time || Tone.Transport.now();
   var i = indexes[2]++ % indexModulos[2];
   if (counters[2][i] === 1 ) {
-    synths[2].triggerAttackRelease('G3', 0.1, Tone.Transport.now(), 0.1);
+    var whichSample = Math.floor(Math.random()*4);
+    var velocity = map( Math.random(), 0, 1, 0.5, 0.75);
+    samplers[2].triggerAttack(String(whichSample), t, velocity);
+
+    // synths[2].triggerAttackRelease('G3', 0.1, Tone.Transport.now(), 0.1);
   }
 }
 
 function play2003(time) {
+  var t = time || Tone.Transport.now();
   var i = indexes[3]++ % indexModulos[3];
   if (counters[3][i] === 1 ) {
-    synths[3].triggerAttackRelease('D4', 0.1, Tone.Transport.now(), 0.1 );
+    var whichSample = Math.floor(Math.random()*4);
+    var velocity = map( Math.random(), 0, 1, 0.5, 0.75);
+    samplers[3].triggerAttack(String(whichSample), t, velocity);
+
+    // synths[3].triggerAttackRelease('D4', 0.1, Tone.Transport.now(), 0.1 );
   }
 }
 
